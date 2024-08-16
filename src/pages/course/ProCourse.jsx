5a9e3c5849da6842df406bd1228911724e243747
iqramahamed <iqra.mahamed64@gmail.com>
2024-08-16 11:38:36 +0300

    added a button

--- src/pages/course/ProCourse.jsx
+++ src/pages/course/ProCourse.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
 import { Spinner, Box, Center, Text } from "@chakra-ui/react";
 import ProCoursesList from "../../components/course/ProCoursesList";
 
