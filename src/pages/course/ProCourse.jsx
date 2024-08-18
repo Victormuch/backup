13b6156b6c1e3c883fe875b93406504a4f112ac1
iqramahamed <iqra.mahamed64@gmail.com>
2024-08-18 21:45:39 +0300

    connected frontend and backend

--- src/pages/course/ProCourse.jsx
+++ src/pages/course/ProCourse.jsx
@@ -13,7 +13,7 @@ function ProCourse() {
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const response = await axios.get("http://127.0.0.1:5000/courses/pro");
+        const response = await axios.get("http://127.0.0.1:5000/pro-courses");
         // Set the response data into courses state
         setCourses(response.data.courses);
         setLoading(false);
